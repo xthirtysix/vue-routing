76a59097d188629cea142cb9f74e3ee41bc8f530
Aleksei Bezdetko <ggalliosha@gmail.com>
2020-06-19 08:50:00 +0300

    Add routes

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
+import routes from './routes';
 
+Vue.use(VueRouter);
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
+const router = new VueRouter({
+  routes,
+  mode: 'history',
+});
+
 new Vue({
   render: (h) => h(App),
+  router,
 }).$mount('#app');
